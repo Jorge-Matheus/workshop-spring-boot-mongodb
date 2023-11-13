c67a4b4ce19fea077ef368b3ab9fb9c62e9f878a
Jorge-Matheus <crossfire_brasil2013@hotmail.com>
2023-11-13 20:33:50 -0300

    database instantiation operation

---
+++ src/main/java/com/jorgematheus/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.jorgematheus.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.jorgematheus.workshopmongo.domain.User;
+import com.jorgematheus.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();;
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+	}
+	
+}
