644bf459f8c757ebb85452cd78b735717692ad87
Jorge-Matheus <crossfire_brasil2013@hotmail.com>
2023-11-14 11:58:58 -0300

    getting user id

--- src/main/java/com/jorgematheus/workshopmongo/services/UserService.java
+++ src/main/java/com/jorgematheus/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jorgematheus.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jorgematheus.workshopmongo.domain.User;
 import com.jorgematheus.workshopmongo.repository.UserRepository;
+import com.jorgematheus.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,6 +21,9 @@ public class UserService {
 		return userRepository.findAll();
 	}
 	
-	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encotrado"));
+	}
 	
 }
