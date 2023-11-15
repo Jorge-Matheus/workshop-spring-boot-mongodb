941fe7217d537631f832e0bf6a4cc57c4cea2ae7
Jorge-Matheus <crossfire_brasil2013@hotmail.com>
2023-11-15 16:30:46 -0300

    Endpoints return posts of Users

--- src/main/java/com/jorgematheus/workshopmongo/resources/UserResource.java
+++ src/main/java/com/jorgematheus/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.jorgematheus.workshopmongo.domain.Post;
 import com.jorgematheus.workshopmongo.domain.User;
 import com.jorgematheus.workshopmongo.dto.UserDTO;
 import com.jorgematheus.workshopmongo.services.UserService;
@@ -60,6 +61,13 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
