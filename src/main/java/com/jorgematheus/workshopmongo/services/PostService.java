2182327a541567f4e1c5a214164f3b5accb22725
Jorge-Matheus <crossfire_brasil2013@hotmail.com>
2023-11-15 17:04:06 -0300

    Getting post ID

---
+++ src/main/java/com/jorgematheus/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.jorgematheus.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jorgematheus.workshopmongo.domain.Post;
+import com.jorgematheus.workshopmongo.repository.PostRespository;
+import com.jorgematheus.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRespository postRepository;
+	
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encotrado"));
+	}
+	
+}
